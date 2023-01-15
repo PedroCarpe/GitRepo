025626c5de2a75da6b30eba90b2850a96e78b61e
Pedro Carpe <dd2426016@gmail.com>
2023-01-15 18:20:56 -0300

    nova atualização em x1.py

--- x1.py
+++ x1.py
@@ -1,10 +1,20 @@
 #A ideia do algoritmo, é simular o cadastro de um cliente/usuario
+import mysql.connector
+
 num=0
 id1=[]
 nome=[]
 cpf=[]
 email=[]
 telefone=[]
+
+#Conexao com o banco de dados
+mybd = mysql.connector.connect(
+host='localhost',
+user='root',
+password='senha',
+database='usuarios'
+)
 
 def criarId():
     global id1
@@ -28,7 +38,7 @@ def criarTelefone():
     global telefone
     telefone.append(input('Digite seu telefone: '))
  
-def chamar():
+def cadastrar():
     criarId()
     criarNome()
     criarCpf()
@@ -47,13 +57,25 @@ def Deletar():
     telefone.pop(pos)
     nome.pop(pos)
 
-    
+########################################################################
+import mysql.connector
+
 print('___Cadastro__de__Cliente___')
 resposta='sim'
 while resposta =='sim':
-    chamar()
+    cadastrar()
     resposta=input('\nQuer cadastrar outro cliente (sim/nao)? ')
-    
+
+vetor = [id1[0],nome[0],cpf[0],email[0],telefone[0]]
+mycursor = mybd.cursor()
+sql = ('INSERT INTO pessoas (id,nome,cpf,email,telefone) VALUES(%s,%s,%s,%s,%s)')
+
+val = (vetor[0],vetor[1],vetor[2],vetor[3],vetor[4])
+mycursor.execute(sql,val)
+
+mybd.commit()
+
+
 resposta=input('\nQuer atualizar seus dados (sim/nao)? ')
 if resposta=='sim':
     try:
@@ -72,4 +94,4 @@ if resposta=='sim':
     
 for t in range(len(id1)):
     print('\n___Dados__Cadastrados___\n'+'Nome: '+nome[t]+'\n'+'CPF: '+cpf[t][0:3]+'.'+cpf[t][3:6]+'.'+cpf[t][6:9]+'-'+cpf[t][9:]+'\n'+'Email: '+email[t]+'\n'+'Telefone: ('+telefone[t][0:2]+') '+telefone[t][2:])    
-     
+  
